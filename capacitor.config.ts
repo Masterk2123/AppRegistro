1ec2df54ba9ddb72397d92f8d69602c697c2560f
Masterk2123 <ke.arucutipa@duocuc.cl>
2023-10-26 23:11:39 -0300

    avances 3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,26 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     androidScheme: 'https'
-  }
-};
-
+  },
+  plugins:{
+    CapacitorSQLite:{
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption:false,
+      iosKeychainPrefx: 'YOUR_APP_NAME',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite"
+      },
+      androidIsEncryption: false,
+      androidBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite",
+        biometricSubtitle:"Log in using your biometric"
+      },
+      electronIsEncryption: false,
+      electronWindowsLocation: "C:\\ProgamData\\CapacitorDatabases",
+      electronLinuxLocation: "Database"
+      }
+    }
+  };
 export default config;
